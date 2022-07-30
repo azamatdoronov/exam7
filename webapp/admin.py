8f491be2c72bdc1f562a86a8ef72df7f3e5edf59
azamatdoronov <aza3el@inbox.ru>
2022-07-30 12:33:59 +0600

    Создал модель Опроса (Poll), Варианта ответа (Choice) и зарегистрировал модели в админ-панели

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_p']
+    list_display_links = ['question']
+    list_filter = ['question']
+    search_fields = ['question']
+    fields = ['question', 'created_p', 'updated_p']
+    readonly_fields = ['created_p', 'updated_p']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
